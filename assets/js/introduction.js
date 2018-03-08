ddda6296345a64615e3a10f221110dd3465a1d39
Michael Lecchino <michaellecchino@Michaels-MacBook-Pro.local>
2018-03-08 22:10:18 +0100

    Minor change to greeting

--- assets/js/introduction.js
+++ assets/js/introduction.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       $(".greetings").html("Hi, I'm <span style='border-bottom:0.35rem solid #FF4756'>Michael</span>.<br><br>A curious student <br>from <span style='border-bottom:0.35rem solid #80FF80'>Montréal</span><br>passionate about <br><span data-typer-targets='design,architecture,typography,photography,cinematography,art,hiking,cooking,travelling,exploring nature,entrepreneurship' style='border-bottom:0.35rem solid #3295FF'>learning</span>.");
     } else {
       // For web (large screen)
-      $(".greetings").html("Hi, I'm <span style='border-bottom:0.35rem solid #FF4756'>Michael</span>—<br>A student from <span style='border-bottom:0.35rem solid #80FF80'>Montréal</span><br>passionate about <span data-typer-targets='design,architecture,typography,photography,cinematography,art,hiking,cooking,travelling,exploring nature,entrepreneurship' style='border-bottom:0.35rem solid #3295FF'>learning</span>.");
+      $(".greetings").html("Hi, I'm <span style='border-bottom:0.35rem solid #FF4756'>Michael</span>.<br>A student from <span style='border-bottom:0.35rem solid #80FF80'>Montréal</span><br>passionate about <span data-typer-targets='design,architecture,typography,photography,cinematography,art,hiking,cooking,travelling,exploring nature,entrepreneurship' style='border-bottom:0.35rem solid #3295FF'>learning</span>.");
     }
   }
 
